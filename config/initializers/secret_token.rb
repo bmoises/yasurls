a503002b2f74e6236a6cfa6ad45915b3ac5a1ec1
Moises Beltran <BMoises@gmail.com>
2010-10-05 22:23:18 -0700

    Added empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yasurls::Application.config.secret_token = 'ae96b83eb3478e3bef1134b47c169b2c86bd237bd203b25326a28dcb9b8dfe111e19200662b689117f8c2dc0d044fc945619439883b05d9df2555a91e3585183'
