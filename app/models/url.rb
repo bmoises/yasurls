d1ef15f858dcfe4a70aa9984f09b05e085f93b28
Moises Beltran <BMoises@gmail.com>
2010-10-06 23:04:53 -0700

    Updated some files

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,8 +2,8 @@ class Url < ActiveRecord::Base
 
   class << self
     def derive_short_url(url)
-      # first determine what the short code will be
-      #url = Url.new(:long_link => url,  :short_link => 'a1b2c3')
+      # first we save to get the record id and use that to determine what the
+      # short url will be
       url = Url.new(:long_link => url)
       # save
       url.save
